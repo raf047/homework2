f1035367258189009f4d4bfa8b93980bb456965e
Raf <rafhovhannisian@gmail.com>
2022-02-06 21:44:29 +0400

    homework2 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,169 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
+//        EX 1
+//        System.out.println("Enter byte number: ");
+//        byte byteNum = scanner.nextByte();
+//        short shortNum = byteNum;
+//        System.out.println(byteNum);
+//        System.out.println(shortNum);
+//
+//        int intNumber = shortNum;
+//        System.out.println(byteNum);
+//        System.out.println(intNumber);
+//
+//        long longNum = intNumber;
+//        System.out.println(byteNum);
+//        System.out.println(longNum);
+//
+//        float floatNum = longNum;
+//        System.out.println(byteNum);
+//        System.out.println(floatNum);
+//
+//        double doubleNum = floatNum;
+//        System.out.println(byteNum);
+//        System.out.println(doubleNum);
+
+//        EX 2
+
+//        System.out.println("Enter double number: ");
+//        double doubleNum = scanner.nextDouble();
+//        float floatNum = (float)doubleNum;
+//        System.out.println(doubleNum);
+//        System.out.println(floatNum);
+//
+//        long longNum = (long)floatNum;
+//        System.out.println(doubleNum);
+//        System.out.println(longNum);
+//
+//        int intNum = (int)longNum;
+//        System.out.println(doubleNum);
+//        System.out.println(intNum);
+//
+//        short shortNum = (short) intNum;
+//        System.out.println(doubleNum);
+//        System.out.println(shortNum);
+//
+//        byte byteNum = (byte) shortNum;
+//        System.out.println(doubleNum);
+//        System.out.println(byteNum);
+
+
+//        EX 3
+
+//        System.out.println("Enter Fahrenheit: ");
+//        int fahTemp = scanner.nextInt();
+//        int celTemp =
+
+//        EX 4
+
+//        System.out.println("Enter radius: ");
+//        double r = scanner.nextDouble();
+//        double area = Math.PI * r * r;
+//        double perimeter = 2 * Math.PI * r;
+//        System.out.println("perimeter is " + perimeter);
+//        System.out.println("area is " + area);
+
+//        EX 5
+
+//        System.out.println((float)1 / 3);
+//        System.out.println((double) 1 / 3);
+
+//        EX 6
+
+//        System.out.print('A');
+//        System.out.print('R');
+//        System.out.print('A');
+//        System.out.print(' ');
+//        System.out.print('1');
+//        System.out.print('9');
+//        System.out.print('9');
+//        System.out.print('6');
+
+//        EX 7
+
+//        char a = 'a';
+//        int castA = a;
+//        System.out.println(a);
+//        System.out.println(castA);
+
+//        EX 8
+
+//        int A = 'A';
+//        int r = 'r';
+//        int a = 'a';
+//        System.out.println((A + r + a) / 3);
+
+//        EX 9
+
+//        System.out.println("Enter integer number: ");
+//        int num = scanner.nextInt();
+//        char symbol = (char) num;
+//        System.out.println(symbol);
+
+//        EX 10
+
+//        String str1 = "some ";
+//        String str2 = "string";
+//        String concStr = str1 + str2;
+//        System.out.println(concStr);
+
+//        EX 11
+
+//        int num1 = 5;
+//        int num2 = 10;
+//        System.out.println(num1 + num2);
+//        String str = "text";
+//        System.out.println(str + num1 + num2);
+
+//        EX 12
+
+//        String txt = "We are the so-called "Vikings" from the north."; // Compile time error
+
+//        String txt = "We are the so-called \"Vikings\" from the north.";
+//        System.out.println(txt);
+
+
+//        TEST
+
+//        EX 1
+
+//        a. Child age - byte
+//        b. Employee salary - int
+//        c .Whether somebody has children or not - boolean
+//        d. Apartment letter - String
+
+//        EX 2
+
+//        a) int a, b, c a = 0 - ";" not exist and we cant use this syntax  int a, a = 0;
+//        b) System.out.println('ab'); - char can have only one symbol
+//        c) System.out.println( (3 + 2) – 1) ); - ) is over
+//        d) int a = 3.2; - 3.2 is double, cant provided to int
+//        e) float a = 2.1; int c = a; - 2.1 is double, cant provided to float, a is float, cant provided to int;
+//        f) int i = 10; float b; i = b; - b isnt initialized
+//        g) int x; { x = 10; } System.out.println(x); - no error
+//        h) boolean a, b; a = false, b = true; - a = false; b = true;
+//        i) int x; x = 1; r = x + 1; - r exist
+//        j) String s = 'This is a string'; - '' is for characters
+
+//        EX 3
+
+//        int i = 10;
+//        int n = i++ % 5;
+//        output will be 0
+
+//        EX 4
+
+//        a) a = 4; b = 12; result = a + b / 3; - 8
+//        b) a = 3; a = a + 3; b = 2; result = a – b; - 4
+//        c) a = 2; b = a + 1; a = b + 2; result = a + b + a; result = -result; - -13
+//        d) a = 3; b = 11; result = (b % a) + 1; - 3
+//        e) a = 3; b = a++; result = 1; result += a – b; - 2
     }
 }
